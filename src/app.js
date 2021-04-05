fad2ca24b84ca4fff71f14e5943efb88f3bc9a0d
Trace Herrell <trace.herrell@gmail.com>
2021-04-04 19:55:20 -0700

    GCP configs

--- src/app.js
+++ src/app.js
@@ -13,9 +13,9 @@ export default function App() {
     <div>
       <AuthCluster />
       <InitCluster address={cu.addr} />
-      <ProfileCluster address={cu.addr} />
-      {/* <ProfileCluster address="0xba1132bc08f82fe2" />
-      <ProfileCluster address="0xf117a8efa34ffd58" /> */}
+      <ProfileCluster address={cu.addr} /> 
+      <ProfileCluster address="0xba1132bc08f82fe2" />
+      <ProfileCluster address="0xf117a8efa34ffd58" />
     </div>
   )
 }
